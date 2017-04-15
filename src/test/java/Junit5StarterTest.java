588e5f34c70ed9d36b3551a4a8d600e127b322d5
holi <32414154@qq.com>
2017-04-15 19:42:13 +0800

    install junit-5 plugin & libraries

---
+++ src/test/java/Junit5StarterTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by holi on 4/15/17.
+ */
+public class Junit5StarterTest {
+
+    @Test
+    void alreadyInstalled() {
+        assertTrue(true);
+    }
+
+}
